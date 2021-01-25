30764a9a31238bc24a104437b40f487da41b0b7a
janisha <65414656+janisha13@users.noreply.github.com>
2021-01-25 13:21:38 -0600

    method exercise

---
+++ Methodsexercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methodsexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name?");
+            String name = Console.ReadLine();
+            Console.Write("What is your favorite color?");
+            String color = Console.ReadLine();
+            Console.Write("What is your favoite animal?");
+            String animal = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {name}, {color} is a great choice. Can you draw a picture of a {animal}, by chance? ");
+            Console.ReadLine();
+        }
+    }
+}
